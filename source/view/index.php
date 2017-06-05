917db49ad3414620cab19ab693fabb5398ed3f6e
sergiodeveloper <sergio-fh@hotmail.com>
2017-06-05 14:57:06 -0300

    Organizando view / Restringindo acesso a arquivos do projeto

--- source/view/index.php
+++ source/view/index.php
@@ -1,38 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <!-- Definir atributos da página -->
-    <meta charset="utf-8" />
-    <meta name="author" content="" />
-    <meta name="description" content="Projeto escolar de uma clínica veterinária utilizando padrão DAO e MVC" />
-    <meta name="keywords" content="clinica,veterinaria" />
-    <title>Clínica Veterinária</title>
-    
-    <!-- Importar estilos e fontes -->
-    <link rel="stylesheet" type="text/css" href="theme.css" />
-    
-    <!-- Importar scripts do projeto -->
-    <!-- ... -->
-    
-    <!-- Importar analytics -->
-    <!-- ... -->
-  </head>
-  
-  <body>
-    <header>
-      <h1>Clínica Veterinária</h1>
-      <nav>
-        <!-- TODO: Menu do site -->
-      </nav>
-    </header>
-    
-    <main>
-      <!-- TODO: conteúdo principal -->
-    </main>
-    
-    <footer>
-      <!-- TODO: rodapé da página -->
-    </footer>
-  </body>
-</html>
+<?php
+
+// Exibir página principal
+
+require_once "home.inc";
 
